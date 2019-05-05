9cf217ee138dd415893c4aaf0da95ff293a383b0
wade <wade.fs@gmail.com>
2019-05-05 11:13:08 +0800

    add phaser3-examples + phaser

---
+++ phaser/src/display/index.js
@@ -0,0 +1,20 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2019 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * @namespace Phaser.Display
+ */
+
+module.exports = {
+
+    Align: require('./align'),
+    BaseShader: require('./shader/BaseShader'),
+    Bounds: require('./bounds'),
+    Canvas: require('./canvas'),
+    Color: require('./color'),
+    Masks: require('./mask')
+
+};
---
+++ phaser/src/renderer/ScaleModes.js
@@ -0,0 +1,47 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2019 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * Phaser Scale Modes.
+ * 
+ * @namespace Phaser.ScaleModes
+ * @memberof Phaser
+ * @since 3.0.0
+ */
+
+module.exports = {
+
+    /**
+     * Default Scale Mode (Linear).
+     * 
+     * @name Phaser.ScaleModes.DEFAULT
+     * @type {integer}
+     * @const
+     * @since 3.0.0
+     */
+    DEFAULT: 0,
+
+    /**
+     * Linear Scale Mode.
+     * 
+     * @name Phaser.ScaleModes.LINEAR
+     * @type {integer}
+     * @const
+     * @since 3.0.0
+     */
+    LINEAR: 0,
+
+    /**
+     * Nearest Scale Mode.
+     * 
+     * @name Phaser.ScaleModes.NEAREST
+     * @type {integer}
+     * @const
+     * @since 3.0.0
+     */
+    NEAREST: 1
+
+};
